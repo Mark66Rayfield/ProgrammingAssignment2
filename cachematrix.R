3f2ea88fbb220043a719223235c9ce52a2555801
Mark Rayfield <mark66rayfield@gmail.com>
2020-06-02 10:49:07 +0800

    Update cachematrix.R
    added descrition to local chacheSolve R script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function designed to store a 2 by 2 matrix and solve its inverse
+## makeCacheMatrix is a repository as a list that stores the 
+## inverse in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve ##diff name?
+  getinv <- function() inv
+  ## list of matrices
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## solveCache retruns the inverse assuming it is solveable
+## there is a check within the function to see if the solution is in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
